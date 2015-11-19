4b24ea06362a3ca0f656ee1675b7716d9a85db73
Marcy Rattner <mar158@uclive.ac.nz>
2015-11-19 16:48:45 +1300

    Enable MySQL connection and automatic migrations

--- Nandoso/Models/NandosoContext.cs
+++ Nandoso/Models/NandosoContext.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
+using MySql.Data.Entity;
 
-namespace Nandoso.Models
-{
-    public class NandosoContext : DbContext
-    {
-        // You can add custom code to this file. Changes will not be overwritten.
-        // 
-        // If you want Entity Framework to drop and regenerate your database
-        // automatically whenever you change your model schema, please use data migrations.
-        // For more information refer to the documentation:
-        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-    
-        public NandosoContext() : base("name=NandosoContext")
-        {
-        }
+namespace Nandoso.Models {
+	public class NandosoContext : DbContext {
+		// You can add custom code to this file. Changes will not be overwritten.
+		// 
+		// If you want Entity Framework to drop and regenerate your database
+		// automatically whenever you change your model schema, please use data migrations.
+		// For more information refer to the documentation:
+		// http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+		public NandosoContext () : base("name=NandosoContext") {
+			Database.SetInitializer(new MigrateDatabaseToLatestVersion<NandosoContext, Config>());
+		}
+
+		/// <summary>
+		/// Configuration class for automatic migrations
+		/// </summary>
+		[DbConfigurationType(typeof(MySqlEFConfiguration))]
+		public class Config : DbMigrationsConfiguration<NandosoContext> {
+			public Config () {
+				this.AutomaticMigrationsEnabled = true;
+			}
+		}
 
 		public System.Data.Entity.DbSet<Nandoso.Models.MenuItem> MenuItems {
 			get; set;
